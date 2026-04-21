473bfc5e6f0bf992e597a1c261e45ed6afcc8d75
faizulmushofa <faizulmushofa1@gmail.com>
2026-04-22 01:54:56 +0700

    refactoring code

--- Phase-2-Core-Features/OrmWithJpa/src/main/java/org/example/ormwithjpa/Service/Interface/AuthService.java
+++ Phase-2-Core-Features/OrmWithJpa/src/main/java/org/example/ormwithjpa/Service/Interface/AuthService.java
@@ -4,7 +4,6 @@ import org.example.ormwithjpa.Dto.request.LoginRequest;
 import org.example.ormwithjpa.Dto.request.RegisterRequest;
 import org.example.ormwithjpa.Dto.response.LoginResponse;
 import org.example.ormwithjpa.Dto.response.RegisterResponse;
-
 public interface AuthService {
 
     LoginResponse login(LoginRequest loginRequest);
